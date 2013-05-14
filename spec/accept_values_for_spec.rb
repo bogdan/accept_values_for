d7dc3b773e7c175801205f6c2b38ecbdade72ae4
Roman Parashchenko <roman.paraschenko@clickworker.com>
2013-05-14 18:35:11 +0300

    Implemented all values validation with errors messages aggregation

--- spec/accept_values_for_spec.rb
+++ spec/accept_values_for_spec.rb
@@ -4,17 +4,20 @@ describe "AcceptValuesFor" do
   subject { Person.new(:gender => "MALE", :group => Group.new(:name => "Primary")) }
 
 
+  it "should have custom condition for should" do
+    accept_values_for(:gender, "INVALID", "MALE").matches?(subject).should be_false
+  end
+
   it "should have custom condition for should_not" do
-    accept_values_for(:gender, "INVALID", "MALE").does_not_match?(subject).should be_false
+    accept_values_for(:gender, "INVALID", "MALE").does_not_match?(subject).should be_true
   end
 
   describe "#matches?" do
-    let(:matcher) { accept_values_for(:gender, "INVALID") }
+    let(:matcher) { accept_values_for(:gender, "INVALID", "WRONG") }
 
     it "should return false if one of values is not accepted" do
       matcher.matches?(subject).should be_false
-      matcher.failure_message_for_should.should == "expected #{subject.inspect} to accept value \"INVALID\" for :gender, but it was not\n" +
-        "Errors: gender is not included in the list"
+      matcher.failure_message_for_should.should == "expected #{subject.inspect} to accept values [\"INVALID\", \"WRONG\"] for :gender, but it was not\n\nValue: INVALID\tErrors: gender is not included in the list\nValue: WRONG\tErrors: gender is not included in the list"
     end
 
     it "should assign the old value for attribute after #matches? " do
