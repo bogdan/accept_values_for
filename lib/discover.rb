3e226a7506f7d5585b7dbe1ec3bcd6050b7434d0
Steve Richert <steve.richert@gmail.com>
2014-02-18 17:14:42 -0500

    Remove outdated "discover" functionality

--- lib/discover.rb
+++
@@ -1,7 +0,0 @@
-if defined?(ActiveRecord) && !respond_to?(:discover)
-  def discover(*objects) #:nodoc:
-    include(*objects)
-  end
-  warn "#discover matcher was removed from accept_values_for. Use gem discover instead"
-end
-
