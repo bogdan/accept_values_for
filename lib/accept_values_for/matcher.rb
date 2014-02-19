5a5d8f47932c7b81ebf0944e3c34b988c9c75129
Steve Richert <steve.richert@gmail.com>
2014-02-19 07:23:12 -0500

    Refactor matching conditions, simplifying logic
    This doesn't yet clean up message building.

--- lib/accept_values_for/matcher.rb
+++ lib/accept_values_for/matcher.rb
@@ -1,6 +1,6 @@
 module AcceptValuesFor
   class Matcher
-    attr_reader :attribute, :values, :model
+    attr_reader :attribute, :values, :model, :errors
 
     def initialize(attribute, *values)
       @attribute = attribute
@@ -8,57 +8,66 @@ module AcceptValuesFor
     end
 
     def matches?(model)
-      base_matches?(model) do |value|
-        unless model.errors[attribute].to_a.empty?
-          failed_values[value] = Array(model.errors[attribute]).join(", ")
-        end
-      end
+      test(model)
+      no_errors?
     end
 
     def does_not_match?(model)
-      base_matches?(model) do |value|
-        if model.errors[attribute].to_a.empty?
-          failed_values[value] = nil
-        end
-      end
+      test(model)
+      all_errors?
     end
 
+    # FIXME
     def failure_message_for_should
       result = "expected #{model.inspect} to accept values #{formatted_failed_values} for #{@attribute.inspect}, but it was not\n"
-      failed_values.keys.sort.each do |key|
-        result << "\nValue: #{key}\tErrors: #{attribute} #{failed_values[key]}"
+      errors.sort.each do |key, errors|
+        result << "\nValue: #{key}\tErrors: #{attribute} #{errors.join(", ")}"
       end
       result
     end
 
+    # FIXME
     def failure_message_for_should_not
       "expected #{model.inspect} to not accept values #{formatted_failed_values} for #{attribute.inspect} attribute, but was"
     end
 
+    # FIXME
     def description
-      "accept values #{values.map(&:inspect).join(', ')} for #{attribute.inspect} attribute"
+      "accept values #{values.map(&:inspect).join(", ")} for #{attribute.inspect} attribute"
     end
 
     private
-    def base_matches?(model)
+
+    def test(model)
       @model = model
-      old_value = model.send(attribute)
+      @errors = {}
+
+      original_value = @model.send(attribute)
+
       values.each do |value|
-        model.send("#{attribute}=", value)
-        model.valid?
-        yield(value)
+        test_value(model, value)
       end
-      return failed_values.empty?
     ensure
-      model.send("#{attribute}=", old_value) if defined?(old_value)
+      @model.send("#{attribute}=", original_value)
     end
 
+    def test_value(model, value)
+      model.send("#{attribute}=", value)
+      model.valid?
+      errors[value] = model.errors[attribute]
+    end
+
+    def no_errors?
+      errors.values.all? { |e| e.empty? }
+    end
+
+    def all_errors?
+      errors.values.all? { |e| !e.empty? }
+    end
+
+    # FIXME
     def formatted_failed_values
-      failed_values.keys.sort.map(&:inspect).join(", ")
-    end
-
-    def failed_values
-      @failed_values ||= {}
+      errors.keys.sort.map(&:inspect).join(", ")
     end
   end
 end
